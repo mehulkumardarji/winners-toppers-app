46c2acf41a3428502a7a166eb2ccda4ca1c59f58
Mehul Kumar Darji <mehultaylor7@gmail.com>
2023-06-25 23:26:28 +0530

    Creating Componenets Users, Toppers and Winners.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UsersComponent } from './users/users.component';
+import { WinnersComponent } from './winners/winners.component';
+import { ToppersComponent } from './toppers/toppers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    WinnersComponent,
+    ToppersComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB40l4ywoRKKO1PMsbMMygsuqM2qzVbiDA",
+    authDomain: "winners-toppers-app.firebaseapp.com",
+    projectId: "winners-toppers-app",
+    storageBucket: "winners-toppers-app.appspot.com",
+    messagingSenderId: "374727173947",
+    appId: "1:374727173947:web:4f4ff3350be20a0fa5391e",
+    measurementId: "G-1ZKX2J7P91"
+  };  
